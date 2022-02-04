f2ce75f38de089f5a680251f9acaf9ddde10c53b
Lokisley Oliveira <lokisley@hotmail.com>
2022-02-03 22:43:09 -0300

    refactor: change tag span to div in header.jsx
    fix className in tmdb-logo.jsx

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,10 +1,10 @@
 function Header() {
   return (
     <header>
-      <span>
         <h1>Milhões de filmes, séries e pessoas para descobrir. Explore já.</h1>
         <h6>FILTRE POR:</h6>
-      </span>
+        <div>
+        </div>
     </header>
   );
 }
