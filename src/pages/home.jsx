99fd3acd40187bfb387aa4c0073c614ae1ccbef5
Lokisley Oliveira <lokisley@hotmail.com>
2022-02-09 22:34:57 -0300

    refactor: remove useless code on home.jsx
    change page title on index.html

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,12 @@
-import React, { useState } from "react";
 import { Nav, Header, Movies } from "../components/index";
 import "./home.scss";
 
 function Home() {
-  const [genres, setGenres] = useState([]);
-
   return (
     <>
       <Nav />
-      <Header filter={genres} setFilter={setGenres} />
-      <Movies filter={genres} />
+      <Header />
+      <Movies />
     </>
   );
 }
